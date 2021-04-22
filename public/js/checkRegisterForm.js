798184da887469a6c8a27aae4a6e6b01c03f5b08
lotiko <l.despres@netc.fr>
2021-04-22 18:10:50 +0200

    add check form register with html

---
+++ public/js/checkRegisterForm.js
@@ -0,0 +1,7 @@
+const $pseudo = document.querySelector('input[name="username"]');
+const $password = document.querySelector('input[name="password"]');
+const $confirm = document.querySelector('input[name="confirm"]');
+const $mail = document.querySelector('input[name="mail"]');
+const $avatar = document.querySelector('input[name="avatar"]');
+
+console.log($pseudo, $password, $confirm, $mail, $avatar);
